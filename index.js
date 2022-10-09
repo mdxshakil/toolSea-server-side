c3b19124a3701d9d9e0f6ea4fd18b6cd5bf2a7ab
mdxshakil <mdxshakil1767@gmail.com>
2022-10-09 21:24:46 +0600

    JWT verification implemented

--- index.js
+++ index.js
@@ -8,8 +8,25 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1kye1ty.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// middle wires
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req,res,next) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message:'Unauthorized Access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,decoded) =>{
+        if (err) {
+            return res.status(403).send({message: 'Forbidden Access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 async function run() {
     try {
@@ -33,21 +50,20 @@ async function run() {
             res.send({ result, token });
         })
         //load specific user details
-        app.get('/user/:email', async(req,res)=>{
+        app.get('/user/:email', verifyJWT, async(req,res)=>{
             const email = req.params.email;
             const query = {email:email};
             const user = await usersCollection.findOne(query);
             res.send(user);
         })
         //get reviews from user
-        app.post('/review', async(req,res)=>{
+        app.post('/review',verifyJWT, async(req,res)=>{
             const review = req.body;
-            console.log(review);
             const result = await reviewsCollection.insertOne(review);
             res.send(result);
         })
         //load all the reviews reverse order with limit
-        app.get('/review', async(req,res)=>{
+        app.get('/review',verifyJWT, async(req,res)=>{
             // const query = {};
             // const reviews = await reviewsCollection.find(query).toArray();
             const reviews = await reviewsCollection.find().sort({$natural: -1 }).limit(6).toArray();
